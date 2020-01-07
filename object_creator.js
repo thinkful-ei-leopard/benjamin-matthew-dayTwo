b8395c23148ac99d5556edf08f0707e43fcd681b
Benjamin Dolan <benjamin@thinkful.com>
2020-01-07 13:30:49 -0700

    completed object_creator + self_reference

--- object_creator.js
+++ object_creator.js
@@ -1,13 +1,8 @@
-function createMyObject(){
-    return{
-        foo: 'bar',
-        answerToUniverse: 42,
-        "olly olly": 'oxen free',
-        sayHello(){
-            return "hello"
-        }
-    }
+function  updateObject(obj){
+    obj.foo = 'foo'
+    obj.bar = 'bar'
+    obj.bizz = 'bizz'
+    obj.bang = 'bang'
+    return obj
 }
-console.log(createMyObject())
-let check = createMyObject()
-console.log(check.sayHello())
+console.log(updateObject({foo:"too", bar:"boo", bizz:"biss"}))
---
+++ self_reference.js
@@ -0,0 +1,14 @@
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName(){
+          return this.firstName + " " + this.lastName
+      }
+    };
+    return person;
+  }
+  let person = personMaker()
+  console.log(person.fullName())
