5346d60a16f0433ebe37e780bc07f7c1194073f8
Benjamin Dolan <benjamin@thinkful.com>
2020-01-08 06:37:06 -0700

    finished arrays_in_objs.js

--- Obj_init_and_methods.js
+++ Obj_init_and_methods.js
@@ -1,3 +1,4 @@
 const loaf = {flour: 300, water: 210, hydration:(210 / 300 * 100)};
 
 console.log(loaf.flour, loaf.water);
+console.log(loaf.hydration);
---
+++ arrays_in_objs.js
@@ -0,0 +1,2 @@
+const hobbitMeals = {meals:["breakfast", "lunch", "afternoon tea", "dinner", "supper"]}
+console.log(hobbitMeals.meals[4])
